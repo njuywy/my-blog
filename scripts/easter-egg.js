7f38dae0f39298bc1595df1571f08125297a27b1
root <root@localhost.localdomain>
2026-01-30 16:37:03 +0800

    取消彩蛋，恢复Tags设置

--- scripts/easter-egg.js
+++
@@ -1,7 +0,0 @@
-// scripts/easter-egg.js
-// 彩蛋：把所有 /tags/ 链接改为游戏页面
-
-hexo.extend.filter.register('after_render:html', function(html, data) {
-    // 替换所有 /tags/ 链接为游戏页面
-    return html.replace(/href="\/tags\//g, 'href="/my-blog/games/');
-});
