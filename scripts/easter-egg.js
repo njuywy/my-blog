b4eb4388f1cd32226605c6e688b6f0b7c3405a2b
root <root@localhost.localdomain>
2026-01-30 16:35:04 +0800

    把所有/tags/链接改为游戏页面

--- scripts/easter-egg.js
+++ scripts/easter-egg.js
@@ -1,19 +1,7 @@
 // scripts/easter-egg.js
-// 彩蛋：修改文章末尾的标签链接为游戏页面
+// 彩蛋：把所有 /tags/ 链接改为游戏页面
 
 hexo.extend.filter.register('after_render:html', function(html, data) {
-    // 把文章标签云的链接改为游戏页面
-    var tagScript = `
-<script>
-(function(){
-  var tagLinks=document.querySelectorAll('.tag-link, .article-tag-link, [class*="tag"] a, .post-tags a, .article-tag a');
-  tagLinks.forEach(function(link){
-    link.href='/my-blog/games/';
-    link.title='点击进入游戏页面！';
-  });
-})();
-</script>
-`;
-    
-    return html.replace('</body>', tagScript + '</body>');
+    // 替换所有 /tags/ 链接为游戏页面
+    return html.replace(/href="\/tags\//g, 'href="/my-blog/games/');
 });
